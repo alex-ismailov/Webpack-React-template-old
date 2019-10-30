01a0973e8d2551fda84110f09135a5dcc7c90976
Alex-Smil <alexsmile39@gmail.com>
2019-10-30 17:48:45 +0300

    Добавил REACT создал тестовый компонент src\components\app.component.js отказался от src\components\app.component.css в пользу отдельного scss файла для текущего блока src\assets\scss\modules\intro.scss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-import './js/testES6'
-import './assets/scss/main.scss'
+import './js/testES6';
+import './assets/scss/main.scss';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app.component";
+ReactDOM.render(<App />, document.querySelector("#root"));
